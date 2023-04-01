f7f62b53d05e392e01ebb0e65d3cb8c07d56ba35
Devansh Gupta <63590510+GraphicsMonster@users.noreply.github.com>
2023-04-01 18:09:00 +0000

    6 big changes and things learnt: 1. starting express servers 2. serving html files 3. serving static files like stylesheets 4. serving json files 5. using environment variables 6. creating root level request handler middlewares.

--- myApp.js
+++ myApp.js
@@ -1,8 +1,30 @@
 let express = require('express');
 let app = express();
+console.log("Hello World");
 
+const filepath = "views/index.html";
 
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
 
+app.get("/", (req, res) => {
+  res.sendfile(filepath);
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+const data = {
+  message: "Hello json"
+}
+
+app.get("/json", (req, res)=>{
+  if(process.env.MESSAGE_STYLE == 'uppercase'){
+    data.message = data.message.toUpperCase();
+  }
+  res.json(data);
+})
 
 
 
